b319675239330ffe8d511279025daed6d93e6e7b
Ivan Bulut <ivan.bulut@gmail.com>
2023-11-15 13:20:26 +0100

    Modified generic Create and Failure constructors

--- 4Create.Domain/Shared/Result.cs
+++ 4Create.Domain/Shared/Result.cs
@@ -21,16 +21,21 @@ public class Result
     public static Result Success() => new(true, Error.None);
     public static Result Failure(Error error) => new(false, error);
     public static Result<TValue> Success<TValue>(TValue value) => new(value, true, Error.None);
-    public static Result<TValue> Failure<TValue>(TValue value, Error error) => new(value, false, error);
+    public static Result<TValue> Failure<TValue>( Error error) => new(default!, false, error);
+    public static Result<TValue> Create<TValue>(TValue value, Error error)
+            where TValue : class
+            => value is null ? Failure<TValue>(error) : Success(value);
 }
 
 public class Result<TValue> : Result
 {
-    private readonly TValue? _value;
+    private readonly TValue _value;
 
     protected internal Result(TValue? value, bool isSuccess, Error error)
         :base(isSuccess, error) 
         =>  _value = value;
 
-    public static implicit operator Result<TValue>(TValue value) => Result.Success(value);
+    public TValue Value => IsSuccess
+            ? _value
+            : throw new InvalidOperationException("The value of a failure result can not be accessed.");
 }
